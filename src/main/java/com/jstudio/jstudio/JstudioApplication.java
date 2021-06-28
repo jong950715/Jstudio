de3af1d086c0b044d7602f30ead1230622e5aadc
Jongsus <pjs0102303@gmail.com>
2021-06-29 00:00:00 +0900

    엔티티 기획하기

--- src/main/java/com/jstudio/jstudio/JstudioApplication.java
+++ src/main/java/com/jstudio/jstudio/JstudioApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JstudioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JstudioApplication.class, args);
 	}
