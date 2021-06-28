0d477a5ee1d5a7d7dee1f037ccfa20d1e9fea22f
Jongsus <pjs0102303@gmail.com>
2021-06-28 23:48:54 +0900

    시작하기

---
+++ src/main/java/com/jstudio/jstudio/JstudioApplication.java
@@ -0,0 +1,13 @@
+package com.jstudio.jstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jstudio/jstudio/JstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jstudio.jstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
